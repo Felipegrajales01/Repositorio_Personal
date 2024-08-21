8ebdb95f5a5b75a11b598ef957bb8990cfacd4f7
felipegrajales01 <felipe.grajales01@uceva.edu.co>
2024-08-20 20:49:10 -0500

    " "

--- Punteros/src/main.cpp
+++ Punteros/src/main.cpp
@@ -1,13 +1,20 @@
 #include <Arduino.h>
 
-int numero = 10; //variable global
+int numero = 10;
 
+void duplicar(int *puntero){
+  *puntero = *puntero*2;
+  Serial.println("{duplicar} numero ="+String(*puntero)); 
+}
 
 void setup() {
-
+    Serial.begin(115200);
 }
 
 void loop() {
-
+  Serial.println("numero = "+String(numero));
+  duplicar(&numero);
+  delay(1000);
 }
+
 
